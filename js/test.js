7f3b695bc509aa50b66d0e394df64dd3eb3422b0
Ruben De Neckere <ruben.de.neckere@student.ehb.be>
2018-05-24 00:07:26 +0200

    ..

--- js/test.js
+++ js/test.js
@@ -1,19 +1,48 @@
 
 
-var baseUrl = "https://c41f0f46-dc80-44f1-a4b4-cfc752fa88b5-bluemix.cloudant.com/test/6fa44601f131e0b0a81d6d9d3af0bbff";
+
 function errorHandler(jqXHR, textStatus, errorThrown) {
     console.log("something went wrong: " + textStatus + " " + errorThrown + " " + JSON.stringify(jqXHR));
 }
 
+var url="https://c41f0f46-dc80-44f1-a4b4-cfc752fa88b5-bluemix.cloudant.com/test/6fa44601f131e0b0a81d6d9d3af0bbff"
+
+var doc;
+
+$.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(data, textStatus, jqXHR) {
+
+        console.log(data);
+        test(data);
+
+
+
+    }
+});
+function test( hallo ){
+    console.log(hallo);
+    doc=hallo;
+    doc.test="hallowereld";
+
+    $.ajax({
+        url: url,
+        type: "PUT",
+        contentType: "application/json",
+        data:doc,
+        error: errorHandler
+    })
+
+}
+
+
 
 $("#hallo").on("click",function (e) {
 
+
 e.preventDefault();
-    var file= JSON.stringify({
-            //_id: "newplaylist",
-            //_rev: "playlist"
-            "test":"hallo"
-    });
+
     $.ajax({
         url: baseUrl,
         type: "PUT",
